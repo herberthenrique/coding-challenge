83d38cf48a1b66b67b690ec6218434a6c398461e
Herbert Henrique <herbert.henrique@sumup.com>
2018-11-19 11:17:19 -0200

    Configure React Testing Library

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(<App />);
+describe('Enzyme testing', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+});
+
+describe('react-testing-library', () => {
+  it('renders welcome message', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Learn React')).toBeInTheDocument();
+  });
 });
